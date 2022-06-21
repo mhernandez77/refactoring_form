463d367fc98063df3a802d20990d49f97aa3425d
Maleni A Hernandez <maleni.alexis@gmail.com>
2022-06-20 21:42:18 -0500

    cleaned up code

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
+// export default addTodo;
 
-
-function App(){
+function App() {
   const [todos, setTodos] = React.useState([
     {
-      text: 'learn react',
+      text: "learn react",
       isCompleted: false,
     },
     {
-      text: 'meet friend for lunch',
+      text: "meet friend for lunch",
       isCompleted: false,
     },
     {
-      text: 'build todo app',
+      text: "build todo app",
       isCompleted: false,
-    }        
+    },
   ]);
 
   //text is the value of the form. state gets acced ffrom the components
@@ -21,34 +21,31 @@ function App(){
   //   const newTodos = [...todos, {text:text, isCompleted:false}];
   //   setTodos(newTodos);
   // }
-  const addTodo = text => {
-    const newTodos = [...todos, {text:value, isCompleted:false}];
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text: text, isCompleted: false }];
     setTodos(newTodos);
-  }
+  };
 
   //was an event -> index bc it's passed into the toDo component function
-  const removeTodo = index => {
-    let temp = [...todos];    
+  const removeTodo = (index) => {
+    let temp = [...todos];
     temp.splice(index, 1);
     setTodos(temp);
-  }
+  };
 
-  return(
-      <div className="app">
-        <div className="todo-list">
-         {todos.map((todo, i) => (
-        //  2 components
-        //  items vs forms in the components
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, i) => (
+          //  2 components
+          //  items vs forms in the components
 
-          <todo index={i} todo={todo} key={i}
-               remove={removeTodo}/>))}
-          <TodoForm addTodo={addTodo}/>
-        </div>
-       </div>
-);
+          <Todo index={i} todo={todo} remove={removeTodo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
